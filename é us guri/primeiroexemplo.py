e5770444f68163cf2dd98be829c32935fc9419bb
LJunker <ljunkerunisinos@gmail.com>
2024-03-06 21:39:39 -0300

    ArquivoPrimeiroExemplo.py criado

---
+++ é us guri/primeiroexemplo.py
@@ -0,0 +1,21 @@
+# comando de saida
+print('fala fiot')
+
+# criando uma variavel numerica e atribuindo um valor
+num = 10
+
+# imprimindo o valor da variavel num
+print (num)
+
+# trocando o valor da variavel num
+num = -1
+
+print (num)
+
+nome = input('digite seu nome e presione ENTER: ')
+print (nome)
+
+#leuitura de valor inteiro para variavel idade    
+idade = int (input('digite sua idade meno: '))
+
+print('sue nome é ',nome, 'esua idade é', idade)
