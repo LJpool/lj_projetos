ba7c3c6c25b0e64be69b2334559d9310edb5e9cc
LJunker <162514032+LJpool@users.noreply.github.com>
2024-03-19 23:42:11 -0300

    Update Ex4.py

--- Lista 1/Ex4.py
+++ Lista 1/Ex4.py
@@ -1,8 +1,23 @@
-pergunta = 'qual o verdadeiro nome do heroi batan?'
-alternativaA = 'Bruce wayne'
+pergunta = 'Qual é o verdadeiro nome do super-herói Batman?'
+alternativaA = 'Bruce Wayne'
 alternativaB = 'Clark Kent'
+alternativaC = 'Peter Parker'
+alternativaD = 'Tony Stark'
+alternativaE = 'Steve Rogers'
 
 print(pergunta)
 print('a) ', alternativaA)
+print('b) ', alternativaB)
+print('c) ', alternativaC)
+print('d) ', alternativaD)
+print('e) ', alternativaE)
 
-#continuar...
+resposta_usuario = input('Digite a letra correspondente à resposta correta: ').lower()
+
+# Verificando a resposta do usuário e exibindo a resposta correta
+resposta_correta = 'a'
+
+if resposta_usuario == resposta_correta:
+    print(f'Você respondeu alternativa {resposta_usuario}. Parabéns! A resposta está correta.')
+else:
+    print(f'Você respondeu alternativa {resposta_usuario}. A resposta correta é a alternativa {resposta_correta}.')
