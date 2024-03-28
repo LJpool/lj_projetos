07f96cd5b9609e4515e4d54795d5fd9f2ca20972
LJunker <162514032+LJpool@users.noreply.github.com>
2024-03-27 21:54:52 -0300

    buh

---
+++ é us guri/2703.py
@@ -0,0 +1,17 @@
+Sexo = 'F'
+idade = 65
+
+if Sexo == 'F':
+    print('é do Sexo feminino')
+    if idade >= 65:
+        print('posui 65 anos ou mais de idade')
+    else:
+        print('posui menos de 65 anos de idade')    
+else:
+    print('não pe do Sexo feminino')
+
+
+if Sexo == 'F' and idade >= 65:
+    print('é do sexo feminino e posui 65 anos de idade')
+
+
---
+++ é us guri/jojo.py
@@ -0,0 +1,8 @@
+while resposta != 's' and resposta != 'n':
+    resposta = input('resposta sim (s) ou não (n): ')
+if resposta == 's':
+    print('como vc respondeu sim.........')
+elif resposta == 'n':
+    print('como vc respondeu não.........')
+else:
+    print('resposta invalida?')
