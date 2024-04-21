90e72667ae259f68fa31dc633ac2f22c4540d538
LJunker <ljunkerunisinos@gmail.com>
2024-04-21 19:44:26 -0300

    .

---
+++ Lista 4/Ex1.py
@@ -0,0 +1,19 @@
+print("Números inteiros de 0 a 100:")
+for i in range(101):
+    print(i, end=" ")
+print("\n")
+
+print("Números pares de 20 a 50:")
+for i in range(20, 51, 2):
+    print(i, end=" ")
+print("\n")
+
+print("Números inteiros de 25 a 70 em ordem decrescente:")
+for i in range(70, 24, -1):
+    print(i, end=" ")
+print("\n")
+
+print("Números ímpares de 25 a 95 em ordem decrescente:")
+for i in range(95, 24, -2):
+    print(i, end=" ")
+print("\n")
