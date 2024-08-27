6af7eeeea0ea99bafdaee4b569ac820ba3f78d53
LJunker <ljunkerunisinos@gmail.com>
2024-08-26 23:36:04 -0300

    ,

--- 2 Segundo_Trimestre/Aula 1/Ex2.py
+++ 2 Segundo_Trimestre/Aula 1/Ex2.py
@@ -1,3 +1,4 @@
+'''
 class data:
     def __init__(self, dia, mes, ano):
         self.dia = dia
@@ -14,3 +15,28 @@ class datateste:
 datateste.executar_teste()
 print
 #rever itens
+'''
+
+# Classe Data
+class Data:
+    # Método construtor que inicializa as variáveis de instância
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    # Método para exibir a data no formato dd/mm/aaaa
+    def displayData(self):
+        return f"{self.dia:02d}/{self.mes:02d}/{self.ano}"
+
+# Aplicativo de teste da classe Data
+class DataTeste:
+    @staticmethod
+    def testar():
+        # Criando uma instância da classe Data
+        data = Data(26, 8, 2024)
+        # Exibindo a data
+        print(data.displayData())
+
+# Executando o teste
+DataTeste.testar()
